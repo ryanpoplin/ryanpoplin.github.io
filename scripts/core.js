8a29c3d5184e1fe19f24270acc6354a52f555579
Ryan Poplin <mobileupstate@gmail.com>
2013-11-12 03:06:28 -0500

    Added more styles and content with the Backbone application.

--- scripts/core.js
+++ scripts/core.js
@@ -5,12 +5,18 @@ var App = {
 };
 
 $(function() {
-	window.articleOne = new App.Models.MainContent();
+	window.articleOne = new App.Models.MainContent({
+		title: "A jQuery Mobile application for Peace Medical Center"
+	});
+	window.articleTwo = new App.Models.MainContent();
+	window.articleThree = new App.Models.MainContent();
 	App.MainContents = new App.Collections.MainContent();
 	App.MainContents.add(window.articleOne);
+	App.MainContents.add(window.articleTwo);
+	App.MainContents.add(window.articleThree);
 	var mainContents = "";
 	App.MainContents.each(function(MainContent) {
-		mainContents += "<div>" + 
+		mainContents += "<div class='main-content'>" + 
 		MainContent.get('title');
 		mainContents += "</div>";
 	});
